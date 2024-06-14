a7d8572a69da773da1ddc5720b3510061ab41f4b
Kyle <johnkylesiat@gmail.com>
2024-06-14 13:20:44 -0500

    3. Models and admin registration
    Created menu and booking models, registered them through admin.py and created mock data to check in MySQL

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    #booking id is automatically created in models
+    booking_name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateField()
+    
+    def __str__(self):
+        return self.booking_name
+
+class Menu(models.Model):
+    #menu id is automatically created in models
+    menu_item = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.menu_item
